4f345d16d7427a8ab1c9ff4e7ebab5549947d660
Rozhnovskaya <irina.rozhnovskaya@sigmaukraine.com>
2019-02-19 16:58:21 +0200

    Add TestData class.

--- integration-tests/src/test/java/com/github/sigma/blog/TestData.java
+++ integration-tests/src/test/java/com/github/sigma/blog/TestData.java
@@ -4,17 +4,18 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.After;
 import org.junit.Test;
 
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
-import static java.util.Collections.singletonMap;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,52 +33,54 @@ public class TestData {
 
     final static WebTarget rootApi = restClient.target("http://127.0.0.1:8080/blog/api/v1");
 
-    int initalSize = 0;
-    UUID uuid = null;
+//        @Test
+        public void test () throws IOException {
 
-    //@Test
-    public void test() {
-        final Map rootApiResponse = jsonBuilder("/").get(Map.class);
-        log.info("rootApiResponse: {}", rootApiResponse.toString());
-        assertThat(rootApiResponse).size().isEqualTo(7);
+//        final Response createPostResponse = jsonBuilder("/posts/create").post(createPostEntity());
+//        log.info("createPostResponse: {}", createPostResponse);
+//
+//        final HashMap post = HashMap.class.cast(createPostResponse.readEntity(HashMap.class));
+//        log.info("post: {}", post);
+//        assertThat(createPostResponse.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
+//        assertThat(post.get("lastModifiedAt")).isNotNull();
+//        assertThat(post.get("body")).isNotNull();
+//        assertThat(post.get("title")).isNotNull();
+//        assertThat(post.get("id")).isNotNull();
 
-        final Collection findAllResponse = jsonBuilder("/posts/find-all").get(Collection.class);
-        log.info("findAllResponse: {}", findAllResponse.toString());
-        int initalSize = findAllResponse.size();
-
-        final Response createPostResponse = jsonBuilder("/posts/create").post(createPostEntity());
-        log.info("createPostResponse: {}", createPostResponse);
-
-        final HashMap post = HashMap.class.cast(createPostResponse.readEntity(HashMap.class));
-        log.info("post: {}", post);
-        assertThat(createPostResponse.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
-        assertThat(post.get("lastModifiedAt")).isNotNull();
-        assertThat(post.get("body")).isNotNull();
-        assertThat(post.get("title")).isNotNull();
-        assertThat(post.get("id")).isNotNull();
-
-        final String draftId = post.get("id").toString();
-        uuid = UUID.fromString(draftId);
     }
+        public static void main(String[] args) throws IOException {
+            File posts = new File("\\resources\\posts");
+            if (posts.isDirectory() && posts.listFiles() != null) {
+                for (File post : posts.listFiles()) {
+                    if (post.exists()) {
+                        final Response createPostResponse = jsonBuilder("/posts/create").post(createPostEntity(post));
+                        log.info("createPostResponse: {}", createPostResponse);
+                    }
+                }
+            }
+            final Collection findAllResponse = jsonBuilder("/posts/find-all").get(Collection.class);
+            log.info("findAllResponse: {}", findAllResponse.toString());
+            System.out.println(findAllResponse.size());
+        }
 
-    @After
-    public void after() {
-        jsonBuilder("/posts/delete").post(deletePostEntity());
-    }
+        private static Entity<HashMap<String, String>> createPostEntity(File file) throws IOException {
+            final HashMap<String, String> payload = new HashMap<>();
+            String title = file.getName();
+            String body = readFile(file.getPath(), Charset.defaultCharset());
+            payload.put("title", title);
+            payload.put("body", body);
+            return Entity.entity(payload, APPLICATION_JSON);
+        }
 
-    private Entity<Map<String, String>> deletePostEntity() {
-        return Entity.entity(singletonMap("id", uuid.toString()), APPLICATION_JSON);
-    }
-
-    private Entity<HashMap<String, String>> createPostEntity() {
-        final HashMap<String, String> payload = new HashMap<>();
-        payload.put("title", "ololo");
-        payload.put("body", "# trololo");
-        return Entity.entity(payload, APPLICATION_JSON);
-    }
-
-    private Invocation.Builder jsonBuilder(String path) {
+        private static Invocation.Builder jsonBuilder (String path){
         final WebTarget webTarget = rootApi.path(path);
         return webTarget.request(APPLICATION_JSON);
+        }
+    public static String readFile(String filePath, Charset encoding) throws IOException {
+
+            byte[] encoded = Files.readAllBytes(Paths.get(filePath));
+            return new String(encoded, encoding);
+
     }
 }
+
